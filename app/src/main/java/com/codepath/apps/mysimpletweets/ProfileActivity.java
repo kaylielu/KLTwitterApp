53d1cd19a7c12dd98b3d2768844cd65e53581fb0
Kaylie Lu <kaylielu8@gmail.com>
2016-06-27 21:16:12 -0700

    profile

--- app/src/main/java/com/codepath/apps/mysimpletweets/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ProfileActivity.java
@@ -3,10 +3,13 @@ package com.codepath.apps.mysimpletweets;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.mysimpletweets.fragments.UserTimelineFragment;
 import com.codepath.apps.mysimpletweets.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONObject;
 
@@ -27,19 +30,36 @@ public class ProfileActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response){
                 user = User.fromJSON(response);
                 // My current user account's info
-                getSupportActionBar().setTitle(user.getScreenName());
+                getSupportActionBar().setTitle("@" + user.getScreenName());
+                populateProfileHeader(user);
             }
         });
+
+
         String screenName = getIntent().getStringExtra("screen_name");
         // Create the user timeline fragment
-        if(savedInstanceState == null){
+        if(savedInstanceState == null) {
 
+
+            UserTimelineFragment fragmentUserTimeline = UserTimelineFragment.newInstance(screenName);
+            // Display user timeline fragment within this activity (dynamically)
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flContainer, fragmentUserTimeline);
+            ft.commit(); // changes the fragments
         }
-        UserTimelineFragment fragmentUserTimeline = UserTimelineFragment.newInstance(screenName);
-        // Display user timeline fragment within this activity (dynamically)
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.flContainer, fragmentUserTimeline);
-        ft.commit(); // changes the fragments
 
+    }
+
+    private void populateProfileHeader(User user) {
+        TextView tvName = (TextView) findViewById(R.id.tvFullName);
+        TextView tvTagline = (TextView) findViewById(R.id.tvTagline);
+        TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
+        TextView tvFollowing = (TextView) findViewById(R.id.tvFollowing);
+        ImageView ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
+        tvName.setText(user.getName());
+        tvTagline.setText(user.getTagline());
+        tvFollowers.setText(user.getFollowersCount() + " Followers");
+        tvFollowing.setText(user.getFollowingsCount() + " Following");
+        Picasso.with(this).load(user.getProfileImageUrl()).into(ivProfileImage);
     }
 }
